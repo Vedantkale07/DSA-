afa400faaa0781c37a3f8919298cc8141332acca
vedant Kale <vedantkale0707@gmail.com>
2026-08-17 20:10:43 +0530

    Arrays program added

---
+++ Swap.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Swap {
+
+    public static void main(String[] args) {
+
+        int [] arr= {2,5,1,4,7};
+        System.out.println("Before swap:");
+        System.out.println(Arrays.toString(arr));
+        swap(arr,0,3);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void swap(int [] arr, int index1, int index2){
+
+        int temp=arr[index1];
+        arr[index1]= arr[index2];
+        arr[index2]= temp;
+
+    }
+}
