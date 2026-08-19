3d512316f87b2ac0b41346577bcb8608497adffa
vedant Kale <vedantkale0707@gmail.com>
2026-08-19 09:50:40 +0530

    arraylist program added

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,6 +1,5 @@
 package Arrays;
 
-public class LinearSearch {
 
     public static void main(String[] args) {
         int [] arr={2,5,77,22,9,11,15,19};
@@ -12,12 +11,9 @@ public class LinearSearch {
     // if element is not present in the array return -1
     public static int search(int[] arr, int target) {
 
-        if(arr.length==0){
-            return -1;
-        }
-
-        for (int i = 0; i <arr.length ; i++) {
-
+            if(arr.length==0){
+                return -1;
+            }
             if(arr[i]==target){
                 return i;
             }
