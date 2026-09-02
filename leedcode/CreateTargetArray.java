92ea19d985755d5a5d5fe580b89f6882e1649d5b
vedant Kale <vedantkale0707@gmail.com>
2026-09-02 20:30:24 +0530

    add array program

---
+++ leedcode/CreateTargetArray.java
@@ -0,0 +1,52 @@
+package leedcode;
+
+    // CREATE TARGET ARRAY IN GIVEN ORDER
+
+//Initially target array is empty.
+//From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+//Repeat the previous step until there are no elements to read in nums and index.
+//Return the target array.
+//
+//It is guaranteed that the insertion operations will be valid.
+
+//Example 1:
+//Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+//Output: [0,4,1,3,2]
+//Explanation:
+//nums       index      target
+//0            0          [0]
+//1            1        [0,1]
+//2            2        [0,1,2]
+// 3            2        [0,1,3,2]
+// 4            1        [0,4,1,3,2]
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateTargetArray {
+
+    public static void main(String[] args) {
+
+        int[]  nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        System.out.println(Arrays.toString( createTargetArray(nums, index)));
+
+    }
+
+    static int [] createTargetArray(int [] nums, int [] index){
+        List<Integer> list= new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+
+        int [] target = new int[list.size()];
+        for (int i = 0; i < target.length; i++) {
+            target[i]= list.get(i);
+        }
+
+        return target;
+    }
+}
