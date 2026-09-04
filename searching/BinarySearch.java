0079e934a9de217f394c47cf96a4cf583cd62106
vedant Kale <vedantkale0707@gmail.com>
2026-09-04 22:08:04 +0530

    add leetcode program

---
+++ searching/BinarySearch.java
@@ -0,0 +1,28 @@
+package searching;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int [] arr={1,3,6,8,11,23,45,55,75,77};
+        int target = 3;
+        System.out.println(search(arr, target));
+    }
+
+    static int search(int [] arr, int target){
+        int start= 0;
+        int end= arr.length;
+
+        while(start<= end) {
+            int mid= start + (end -start)/2;
+            if (target < arr[mid]){
+                end= mid-1;
+            }else if(target > arr[mid]){
+                start = mid +1;
+            }else if (target== arr[mid]){
+                return mid;
+            }
+        }
+        // if target element is not present in the array return -1
+        return -1;
+    }
+}
--- searching/LinearSearch.java
+++ searching/LinearSearch.java
@@ -1,6 +1,4 @@
-package Arrays;
-
-import java.util.Arrays;
+package searching;
 
 public class LinearSearch {
 
